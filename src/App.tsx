5759b1ad84a3eb212ee275372eb2e2db759d30bb
Kaushal-SF <kaushal.p@simformsolutions.com>
2022-02-14 15:18:10 +0530

    added files/dev

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div >
+        <h2>Hello World!</h2>
+        <h3>React-TS application setup using Webpack</h3>
+    </div>
+  )
+}
+
+export default App;
+
